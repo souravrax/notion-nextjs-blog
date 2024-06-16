136261f90db320a2dc5cbb58581c8d528082386e
Sourav Rakshit <rakshitsourav3@gmail.com>
2024-06-16 13:08:29 +0530

    added google analytics, vercel analytics, vercel speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
 import Hero from "@/components/pages/home/hero";
-import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardContent,
-    CardFooter,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights as VercelSpeedInsights } from "@vercel/speed-insights/next";
+
+const gaId = process.env.GOOGLE_ANALYTICS_ID as string;
 
 export default function Home() {
     return (
         <>
+            <GoogleAnalytics gaId={gaId} />
+            <VercelAnalytics />
+            <VercelSpeedInsights />
             <Hero />
         </>
     );
