6274b62a0ffbcc009fdf206a0327126a6573b5a6
Sourav Rakshit <rakshitsourav3@gmail.com>
2024-09-20 01:21:09 +0530

    statically generate all the blogs

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -1,8 +1,18 @@
 import Blog from "@/components/Blog";
 import BlogLoading from "@/components/BlogLoadingAnimate";
-import Comments from "@/components/Comments/Comments";
-import Reactions from "@/components/Reaction/Reactions";
+import { getBlogs } from "@/lib/helpers/api";
+// import Comments from "@/components/Comments/Comments";
+// import Reactions from "@/components/Reaction/Reactions";
 import { Suspense } from "react";
+
+export const dynamicParams = true
+// Dynamic segments not included in generateStaticParams are generated on demand.
+
+export async function generateStaticParams() {
+  const blogs = await getBlogs();
+  console.log('Generating static blogs')
+  return blogs.map((blog) => ({ blogId: blog.id }));
+}
 
 export default function page({
   params: { blogId },
@@ -15,9 +25,9 @@ export default function page({
       <Suspense fallback={<BlogLoading />}>
         <Blog blogId={blogId} />
       </Suspense>
-      <Suspense fallback={<BlogLoading />}>
+      {/* <Suspense fallback={<BlogLoading />}>
         <Reactions blogId={blogId} />
-      </Suspense>
+      </Suspense> */}
       {/* <Suspense fallback={<BlogLoading />}>
         <Comments id={blogId} />
       </Suspense> */}
